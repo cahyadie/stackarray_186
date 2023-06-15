59cae8cf3cd393674985a45414c08b98a33ac22c
cahyadie <cahyadiilham12@gmail.com>
2023-06-15 15:09:46 +0700

    make void display

--- stackarray_186/stackarray_186.cpp
+++ stackarray_186/stackarray_186.cpp
@@ -36,6 +36,17 @@ public:
 	bool Empty() {
 		return (top == -1);
 	}
-	
+	void display() {
+		if (Empty()) {
+			cout << "\nStack is empty. " << endl;
+		}
+		else {
+			for (int tmp = 0; tmp <= top; tmp++) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
+
+	}
 }; 
+
 
