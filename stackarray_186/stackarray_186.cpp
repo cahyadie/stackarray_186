09923a31714509a82b4f5181d7e2627b54c1702b
cahyadie <cahyadiilham12@gmail.com>
2023-06-15 15:00:25 +0700

    make public in class tackarray

--- stackarray_186/stackarray_186.cpp
+++ stackarray_186/stackarray_186.cpp
@@ -5,5 +5,23 @@ class Stackarray {
 private:
 	string stack_array[5];
 	int top;
+public:
+	//constructor 
+	Stackarray() {
+		top = -1;
+	}
 
+	string push(string element) {
+		if (top == 4) {//step 1
+			cout << "Number of data exeeds the limit." << endl;
+			return "";
+		}
+
+		top++; //step 2
+		stack_array[top] = element; //step 3
+		cout << endl;
+		cout << element << " Ditambahkan(pushed) " << endl;
+
+		return element;
+	}
 }; 
